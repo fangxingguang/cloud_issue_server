47a33c5b6eee886d5e9c11b77e69bdee743b4f77
fangxinggguang <464229614@qq.com>
2016-12-14 23:58:12 +0800

    用户管理

---
+++ application/index/controller/User.php
@@ -0,0 +1,34 @@
+<?php
+namespace app\index\controller;
+use \think\Request;
+use \think\Db;
+class User
+{
+    public function add()
+    {
+        $param = Request::instance()->param();
+        Db::table('user')->insert($param);
+        $user_id = Db::name('user')->getLastInsID();
+        $result = Db::table('user')->where('user_id',$user_id)->find();
+        return $result;
+    }
+
+    public function select()
+    {
+        $user_list = Db::table('user')->field('user_id,user_name')->order('user_create_time desc')->select();
+        return $user_list;
+    }
+
+    public function update()
+    {
+        $param = Request::instance()->param();
+        $result = Db::table('user')->where('user_id',$param['user_id'])->update($param);
+        return $result;
+    }
+
+    public function delete($user_id='')
+    {
+        return Db::table('user')->delete($user_id);
+    }
+    
+}
