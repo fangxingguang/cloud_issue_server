01b4cc11e800f12ff22bfc9409d9a4e08dac20f4
fangxinggguang <464229614@qq.com>
2017-01-08 00:01:00 +0800

    '允许跨域'

--- public/index.php
+++ public/index.php
@@ -10,7 +10,9 @@
 // +----------------------------------------------------------------------
 
 // [ 应用入口文件 ]
-
+header('Access-Control-Allow-Origin: '.$_SERVER['HTTP_ORIGIN']);
+header("Access-Control-Allow-Methods: POST, GET, OPTIONS, PUT, DELETE");
+header("Access-Control-Allow-Credentials:true");
 // 定义应用目录
 define('APP_PATH', __DIR__ . '/../application/');
 // 加载框架引导文件
