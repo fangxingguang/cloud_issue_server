0b18a4fdfa2dd884dce91fa800dfcf9600d992c3
fangxingguang <fangxingguang@163.com>
2018-04-05 22:03:12 +0800

    增加任务分配功能

---
+++ application/index/controller/Job.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace app\index\controller;
+
+use \think\Request;
+use \think\Db;
+
+class Job extends Base
+{
+    public function add()
+    {
+        $params = Request::instance()->param();
+        if (empty($params['id'])) {
+            $params['create_time'] = dateline();
+            $params['update_time'] = dateline();
+            Db::table('job')->insert($params);
+        } else {
+            $params['update_time'] = dateline();
+            Db::table('job')->update($params);
+        }
+        return success('操作成功！');
+    }
+
+    public function select()
+    {
+        //查询未分组的
+        $list1 = Db::table('job')->where(['group_id' => 0])->select();
+        //分组查询
+        $list2 = [];
+        $groupList = Db::table('job_group')->select();
+        foreach ($groupList as $group) {
+            $arr = [];
+            $taskList = Db::table('job')->where(['group_id' => $group['id']])->select();
+            foreach ($taskList as $task) {
+                $arr[$task['status']][] = $task;
+            }
+            $list2[] = [
+                'id' => $group['id'],
+                'name' => $group['name'],
+                'uid' => $group['uid'],
+                'taskList' => $arr
+            ];
+        }
+        $result = [
+            'list1' => $list1,
+            'list2' => $list2,
+        ];
+        return success($result);
+    }
+
+    public function delete()
+    {
+        $params = Request::instance()->param();
+        Db::table('job')->delete($params);
+        return success('操作成功！');
+    }
+
+    public function groupAdd()
+    {
+        $params = Request::instance()->param();
+        $user = Db::table('user')->where(['user_id' => $params['uid']])->find();
+        $params['name'] = $user['user_name'];
+        if (empty($params['id'])) {
+            Db::table('job_group')->insert($params);
+        } else {
+            Db::table('job_group')->update($params);
+        }
+        return success('操作成功！');
+    }
+
+    public function groupDelete()
+    {
+        $params = Request::instance()->param();
+        Db::table('job_group')->delete($params);
+        return success('操作成功！');
+    }
+
+    public function move()
+    {
+        $params = Request::instance()->param();
+        $params['update_time'] = dateline();
+        Db::table('job')->update($params);
+
+        $group = Db::table('job_group')->where('id',$params['group_id'])->find();
+
+        push_msg($group['uid'],1,'你有新的工作啦！');
+        push_msg('all',5,'');
+
+        return success('操作成功！');
+    }
+
+
+}
---
+++ cloud_issue.sql
@@ -0,0 +1,138 @@
+/*
+SQLyog Ultimate v11.13 (64 bit)
+MySQL - 5.6.25-log : Database - cloud_issue
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`cloud_issue` /*!40100 DEFAULT CHARACTER SET utf8 */;
+
+USE `cloud_issue`;
+
+/*Table structure for table `card` */
+
+DROP TABLE IF EXISTS `card`;
+
+CREATE TABLE `card` (
+  `card_id` int(11) NOT NULL AUTO_INCREMENT,
+  `card_name` varchar(20) NOT NULL,
+  `card_order` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `group_id` int(11) DEFAULT NULL,
+  `card_owner` varchar(64) DEFAULT NULL,
+  PRIMARY KEY (`card_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=50 DEFAULT CHARSET=utf8;
+
+/*Table structure for table `group` */
+
+DROP TABLE IF EXISTS `group`;
+
+CREATE TABLE `group` (
+  `group_id` int(11) NOT NULL AUTO_INCREMENT,
+  `group_name` varchar(20) DEFAULT NULL,
+  `group_des` varchar(256) DEFAULT NULL,
+  `group_create_time` datetime DEFAULT NULL,
+  `user_id` int(11) DEFAULT NULL,
+  `group_parter` varchar(1024) DEFAULT NULL,
+  PRIMARY KEY (`group_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8;
+
+/*Table structure for table `job` */
+
+DROP TABLE IF EXISTS `job`;
+
+CREATE TABLE `job` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `title` varchar(256) DEFAULT NULL COMMENT '标题',
+  `start_time` date DEFAULT NULL COMMENT '开始时间',
+  `end_time` date DEFAULT NULL COMMENT '结束时间',
+  `content` text COMMENT '内容',
+  `create_time` datetime DEFAULT NULL,
+  `update_time` datetime DEFAULT NULL,
+  `status` tinyint(4) DEFAULT '0' COMMENT '0未开始 1进行中 2已完成',
+  `group_id` int(11) DEFAULT '0' COMMENT '所属组',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8;
+
+/*Table structure for table `job_group` */
+
+DROP TABLE IF EXISTS `job_group`;
+
+CREATE TABLE `job_group` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(64) DEFAULT NULL COMMENT '组名',
+  `uid` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=utf8;
+
+/*Table structure for table `log` */
+
+DROP TABLE IF EXISTS `log`;
+
+CREATE TABLE `log` (
+  `log_id` int(11) NOT NULL AUTO_INCREMENT,
+  `log_info` varchar(100) DEFAULT NULL,
+  `user_id` int(11) DEFAULT NULL,
+  `log_create_time` datetime DEFAULT NULL,
+  `log_sql` varchar(200) DEFAULT NULL,
+  PRIMARY KEY (`log_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=9314 DEFAULT CHARSET=utf8;
+
+/*Table structure for table `push_log` */
+
+DROP TABLE IF EXISTS `push_log`;
+
+CREATE TABLE `push_log` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `card_id` int(11) DEFAULT NULL,
+  `push_address` varchar(32) DEFAULT NULL,
+  `branch_name` varchar(256) DEFAULT NULL,
+  `result` text,
+  `create_time` datetime DEFAULT NULL,
+  `create_user` varchar(32) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=978 DEFAULT CHARSET=utf8;
+
+/*Table structure for table `task` */
+
+DROP TABLE IF EXISTS `task`;
+
+CREATE TABLE `task` (
+  `task_id` int(11) NOT NULL AUTO_INCREMENT,
+  `card_id` int(11) NOT NULL,
+  `task_name` varchar(100) NOT NULL,
+  `task_project` varchar(100) NOT NULL DEFAULT '' COMMENT '项目名',
+  `task_branch` varchar(100) DEFAULT NULL COMMENT '分支名',
+  `task_des` text NOT NULL,
+  `task_file` text,
+  `task_level` int(11) NOT NULL,
+  `task_rate` int(11) DEFAULT '0' COMMENT '完成度',
+  `task_create_time` datetime NOT NULL,
+  `task_status` int(11) NOT NULL,
+  `user_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`task_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=753 DEFAULT CHARSET=utf8;
+
+/*Table structure for table `user` */
+
+DROP TABLE IF EXISTS `user`;
+
+CREATE TABLE `user` (
+  `user_id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_name` varchar(64) NOT NULL,
+  `user_email` varchar(64) DEFAULT NULL,
+  `user_create_time` datetime NOT NULL,
+  PRIMARY KEY (`user_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=34 DEFAULT CHARSET=utf8;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
